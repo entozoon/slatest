a2d97c7a91a5a9f35d13ada0ba6891cf258e18d1
Myke <michael.cook@techdept.co.uk>
2021-01-08 12:17:22 +0000

    Live reload update not enough tbh

--- bin/slatest.js
+++ bin/slatest.js
@@ -75,7 +75,7 @@ if (options["delete-entire-theme"]) {
         },
       },
       // https: true // moot, as is infered from proxy
-      reloadDelay: 500, // doesn't work without this. No idea why! We need a beefy one regardless, as Shopify is slow
+      reloadDelay: 1000, // doesn't work without this. No idea why! We need a beefy one regardless, as Shopify is slow
       // injectChanges: false
       logLevel: "info",
       logPrefix: "refresh",
